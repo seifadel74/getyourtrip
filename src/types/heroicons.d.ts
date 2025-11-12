45b987935112817eec68333666efabca4cd9c906
seifadel74 <89112205+seifadel74@users.noreply.github.com>
2025-11-12 03:36:56 +0200

    Delete src directory

--- src/types/heroicons.d.ts
+++
@@ -1,8 +0,0 @@
-declare module '@heroicons/react/outline' {
-  import { ComponentType, SVGProps } from 'react';
-  
-  export const SearchIcon: ComponentType<SVGProps<SVGSVGElement>>;
-  export const MapPinIcon: ComponentType<SVGProps<SVGSVGElement>>;
-  export const StarIcon: ComponentType<SVGProps<SVGSVGElement>>;
-  // Add other icons as needed
-}
